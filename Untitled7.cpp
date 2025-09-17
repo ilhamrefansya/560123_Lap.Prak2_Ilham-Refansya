483ab5d64ec8b0336ae8b4eb6c28fb62528c9b5d
ilhamrefansya <ilhamrefansya@gmail.com>
2025-09-17 11:09:42 +0700

    Tugas 1.cpp

--- Untitled7.cpp
+++ Untitled7.cpp
@@ -4,19 +4,18 @@ using namespace std;
 int main() {
 	
 	int A, B, C, D, E, F, G, H, I;
-	cout << "masukkan baris 1" << endl;
-	cin >> A >> B >> C >> endl;
-	cout << "masukkan baris 2" << endl;
-	cin >> D >> E >> F >> endl;
-	cout << "masukkan baris 3" << endl;
-	cin >> G >> H >> I >> endl;
+	cout << "Masukkan baris 1(pisahkan spasi): " << endl;
+	cin >> A >> B >> C;
+	cout << "Masukkan baris 2(pisahkan spasi): " << endl;
+	cin >> D >> E >> F;
+	cout << "Masukkan baris 3(pisahkan spasi): " << endl;
+	cin >> G >> H >> I;
 	cout << right;
-	cout << setw(3) << A << setw(3) << B << setw(3) << C << endl;
-	cout << setw(3) << D << setw(3) << E << setw(3) << F << endl;
-	cout << setw(3) << G << setw(3) << H << setw(3) << I << endl;
+	cout << "Matriks anda: " << endl;
+	cout << setw(3) << A << setw(3) << B << setw(3) << C << setw(3) << endl;
+	cout << setw(3) << D << setw(3) << E << setw(3) << F << setw(3) << endl;
+	cout << setw(3) << G << setw(3) << H << setw(3) << I << setw(3) << endl;
 	
 	return 0;
-	
-	
 	
 }
